0e01914b5df0a0ce4722a30af18028cde24225a0
riturajmahapatra <67279998+riturajmahapatra@users.noreply.github.com>
2023-07-16 21:28:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const crsr = document.querySelector("#cursor")
 const blr = document.querySelector("#cursor-blur")
-document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+ "px";
-    crsr.style.top = dets.y+ "px";
-    blr.style.left = dets.x- 100+"px";
-    blr.style.top = dets.y- 100+ "px";
+document.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x + "px";
+    crsr.style.top = dets.y + "px";
+    blr.style.left = dets.x - 100 + "px";
+    blr.style.top = dets.y - 100 + "px";
 })
 
-gsap.to("#nav",{
+gsap.to("#nav", {
     backgroundColor: "#000",
-    height:"60px",
-    duration:0.5,
+    height: "20px",
+    duration: 0.5,
     scrollTrigger: {
-        trigger:"#nav",
-        scroller:"body",
+        trigger: "#nav",
+        scroller: "body",
         // markers: true,
-        start:"top -10%",
-        end: "top -11%",
+        start: "top -10%",
+        end: "top 15%",
         scrub: 1.5 //it works as a loop
     }
-    
+
 })
 
-gsap.to("#main",{
-    backgroundColor:'#000',
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
+gsap.to("#main", {
+    backgroundColor: '#000',
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
         // markers:true,
-        start:"top -30%",
-        end:"top -70%",
-        scrub: 1.5 
+        start: "top -20%",
+        end: "top -70%",
+        scrub: 1.5
     }
 })
