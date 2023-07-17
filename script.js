64566ea3333bba74ff4f1c4ecbd0442a34e7a693
riturajmahapatra <67279998+riturajmahapatra@users.noreply.github.com>
2023-07-17 23:07:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,16 +7,29 @@ document.addEventListener("mousemove", function (dets) {
     blr.style.top = dets.y - 100 + "px";
 })
 
+var h4All = document.querySelectorAll("#nav h4")
+h4All.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        crsr.style.scale = 4;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function () {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #c3fd5e";
+        crsr.style.backgroundColor = " #c3fd5e"
+    })
+})
+
 gsap.to("#nav", {
     backgroundColor: "#000",
-    height: "20px",
     duration: 0.5,
     scrollTrigger: {
         trigger: "#nav",
-        scroller: "body",
+        scroller: "html",
         // markers: true,
-        start: "top -10%",
-        end: "top 15%",
+        start: "top -5%",
+        end: "top 25%",
         scrub: 1.5 //it works as a loop
     }
 
@@ -32,4 +45,60 @@ gsap.to("#main", {
         end: "top -70%",
         scrub: 1.5
     }
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4, 
+    scrollTrigger:{
+        trigger:"#about-us img",
+        scroller:"body",
+        // markers:true,
+        scrub:2
+    }      
+
+})
+gsap.from(".card",{
+    scale:1.1,	
+    opacity:0,
+    duration:1,
+    stagger:0.09,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        scrub:0.3
+    }      
+
+})
+
+gsap.from("#img1",{
+    y:-70,
+    x:-70,	
+    duration:1,
+    scrollTrigger:{
+        trigger:"#img1",
+        scroller:"body",
+        // markers:true,
+        start: "top 55%",
+        end: "top 45%",
+        scrub:4
+    }      
+
+})
+gsap.from("#img2",{
+    y:70,
+    x:70,	
+    duration:1,
+    scrollTrigger:{
+        trigger:"#img1",
+        scroller:"body",
+        // markers:true,
+        start: "top 55%",
+        end: "top 45%",
+        scrub:4
+    }      
+
 })
